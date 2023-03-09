637531924f7e60ea9e882a20c13ed881924012a9
George Karamanis <gkaramanis1@outlook.com>
2023-03-09 10:53:06 -0500

    removed data

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -10,10 +10,10 @@ export default function Contact() {
     <div className="content">
       <h1 style={{fontWeight: 'bold', fontSize: '25px'}}>Contact</h1>
       <p>
-        <a href="mailto:giorgokaramanis@gmail.com"><FaTelegramPlane size={32} color="whitesmoke" /></a>
+        <a href=""><FaTelegramPlane size={32} color="whitesmoke" /></a>
         <br></br>
         <br></br>
-        <a href="tel:407417497"><FaPhoneAlt size={32} color="whitesmoke"/></a>
+        <a href=""><FaPhoneAlt size={32} color="whitesmoke"/></a>
       </p>
     </div>
     </div>
